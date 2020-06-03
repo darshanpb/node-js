b53170c435efcdaa80ee5dade6e3f47b2dad7e02
darshanpb <darshanpb111@gmail.com>
2020-06-03 00:59:57 -0700

    Trial mach

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
-//var todoController = require('./controllers/todocontroller')
+
 // set up template engine
 app.set('view engine', 'ejs');
 
 //static files
 app.use(express.static('./public'));
-//fire controllers
-//todoController(app);
+app.use(express.static('./machstatz'));
 
 //Listen to the port
 app.listen(process.env.PORT || 3000, function(){
